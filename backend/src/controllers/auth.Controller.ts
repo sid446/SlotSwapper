4f4b1cd941e54e18418f56d978d965f79798bb6b
sid446 <162982475+sid446@users.noreply.github.com>
2025-11-06 12:35:13 +0530

    fixed code

--- backend/src/controllers/auth.Controller.ts
+++ backend/src/controllers/auth.Controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import User from '../models/User';
+import User from '../models/User.js';
 
 const generateToken = (userId: string): string => {
   return jwt.sign({ userId }, process.env.JWT_SECRET!, {
